3de076c1e90febfad44064fc1022ab975481d0de
Giering <NB94559@DOW.COM>
2020-09-30 21:59:57 -0400

    kewl

--- wp-content/themes/mdbtheme/components/navbar.inc.php
+++ wp-content/themes/mdbtheme/components/navbar.inc.php
@@ -28,7 +28,25 @@
         <li class="nav-item">
           <a class="nav-link" href="#contact" data-offset="65">Contact</a>
         </li>
-      </ul><div class="container-fluid saint-petersburg-gradient d-flex align-items-end justify-content-center" id="bg">
+      </ul>
+      <!-- Social Icon  -->
+      <ul class="navbar-nav nav-flex-icons">
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.facebook.com/allamericanmi" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.twitter.com/AllAmericanMich" target="_blank"><i class="fab fa-twitter"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.instagram.com/AllAmericanMI" target="_blank"><i class="fab fa-instagram"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</header>
+<div class="container-fluid saint-petersburg-gradient d-flex align-items-end justify-content-center" id="bg">
   <!--Intro row-->
   <div class="row justify-content-around" id="bg-intro-row">
 
@@ -70,72 +88,3 @@
   </div>
   <!--/Intro row-->
 </div>
-      <!-- Social Icon  -->
-      <ul class="navbar-nav nav-flex-icons">
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.facebook.com/allamericanmi" target="_blank"><i class="fab fa-facebook-f"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.twitter.com/AllAmericanMich" target="_blank"><i class="fab fa-twitter"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.instagram.com/AllAmericanMI" target="_blank"><i class="fab fa-instagram"></i></a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<!-- Intro Section -->
-<div class="container-fluid saint-petersburg-gradient d-flex align-items-end justify-content-center" id="bg">
-  <!--Intro row-->
-  <div class="row justify-content-around" id="bg-intro-row">
-
-    <!--Images column-->
-    <div class="col-xl-4 d-none d-xl-block">
-
-      <picture>
-        <source srcset="https://mdbootstrap.com/img/Marketing/products/background-intro/jquery/pro/2.webp" type="image/jpg">
-        <img src="https://mdbootstrap.com/img/Marketing/products/background-intro/jquery/pro/2.png" class="img-fluid z-depth-1 ml-5" id="bg-photo-1">
-      </picture>
-
-      <picture>
-        <source srcset="https://mdbootstrap.com/img/Marketing/products/background-intro/jquery/pro/1.webp" type="image/jpg">
-        <img src="https://mdbootstrap.com/img/Marketing/products/background-intro/jquery/pro/1.png" class="img-fluid z-depth-1" id="bg-photo-2">
-      </picture>
-
-    </div>
-    <!--/Images column-->
-
-    <!--CTA column-->
-    <div class="col-xl-7 text-xl-left text-center justify-content-center d-flex align-items-center pt-lg-5">
-      <div>
-        <p class="h1-responsive indigo-text mb-0 pb-0">MATERIAL DESIGN FOR <span class="font-weight-bold">BOOTSTRAP</span>
-        </p>
-
-
-        <p class="h4-responsive dark-grey-text">Lifetime usage. 12 months of premium support and unlimited
-          updates
-        </p>
-
-        <a class="btn btn-indigo white-text rounded ml-0 waves-effect waves-light" id="modalActivate" type="button" data-toggle="modal" data-target="#modalPricing" data-pricing-content="mdb-pro-jq"><i class="far fa-gem mr-2"></i>BUY NOW ($99)
-           
-        </a>
-        <a target="_blank" href="https://mdbootstrap.com/docs/jquery/components/demo/" class="btn btn-outline-indigo rounded waves-effect waves-light"><i class="fas fa-external-link-alt mr-2"></i>QUICK PRO
-          DEMO</a>
-          
-        <hr>
-        <p class="dark-grey-text mb-0"><span class="font-weight-bold">jQuery version</span> [standard
-          Bootstrap]
-        </p>
-        <p class="grey-text" id="technology-links">
-          <small>Change version to: <a class="text-danger" href="/pricing/angular/pro/">Angular</a> | <a class="text-info" href="/pricing/react/pro/">React</a> | <a class="green-text" href="/pricing/vue/pro/">Vue</a></small></p>
-      </div>
-    </div>
-    <!--/CTA column-->
-
-  </div>
-  <!--/Intro row-->
-</div>
-
-</header>
