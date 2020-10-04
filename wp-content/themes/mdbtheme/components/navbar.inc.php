4249d41ce8bbdb0fbc69003bfa736158fbcec3bf
Rob Giering <robgiering@gmail.com>
2020-10-04 16:50:48 -0400

    nav

--- wp-content/themes/mdbtheme/components/navbar.inc.php
+++ wp-content/themes/mdbtheme/components/navbar.inc.php
@@ -47,7 +47,27 @@
 
 </header>
 <section>
-      <div class="">
-        <img src="/wp-content/uploads/2020/10/paula-park-e1601762058686.jpg" style="width: 100%">
+<div class="view jarallax" data-jarallax='{"speed": 0.2}'
+      style="background-image: url(/wp-content/uploads/2020/10/paula-park-e1601762058686.jpg); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+      <div class="mask rgba-black-light">
+        <div class="container h-100 d-flex justify-content-center align-items-center">
+          <div class="row pt-5 mt-3">
+            <div class="col-md-12">
+              <div class="text-center">
+                <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-3 wow fadeInDown"
+                  data-wow-delay="0.3s">
+                  <strong>Looking for a pet sitter?</strong>
+                </h1>
+                <hr class="hr-light mt-4 wow fadeInDown" data-wow-delay="0.4s">
+                <h5 class="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s">
+                  <strong>Travel & photography</strong>
+                </h5>
+                <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">Call Now</a>
+                <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
 </section>
