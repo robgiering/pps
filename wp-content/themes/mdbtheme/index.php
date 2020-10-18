f58976ac41dc2414e18509aaa949433d3d99c10c
Giering <NB94559@DOW.COM>
2020-10-18 09:17:02 -0400

    asdf

--- wp-content/themes/mdbtheme/index.php
+++ wp-content/themes/mdbtheme/index.php
@@ -101,7 +101,7 @@
                                     <div class="col-lg-4 col-sm-6 mb-5">
                                         <div class="row d-flex align-items-center">
                                             <div class="col-5 avatar w-100 white d-flex justify-content-center align-items-center">
-                                                <a data-toggle="modal" data-target=".bd-example-modal-lg"><img src="/wp-content/uploads/2020/10/paula.jpg" class="img-fluid rounded-circle z-depth-1" /></a>
+                                                <a data-toggle="modal" data-target=".paula-intro"><img src="/wp-content/uploads/2020/10/paula.jpg" class="img-fluid rounded-circle z-depth-1" /></a>
                                             </div>
                                             <div class="col-7">
                                                 <h6 class="font-weight-bold pt-2">Paula Elandt</h6>
@@ -1158,7 +1158,63 @@
     aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
-        ...
+        <section>
+    
+            <div class="card mb-4">
+              
+              <div class="row">
+        
+                <div class="col-md-6">
+                  <img class="img-fluid rounded-left" src="https://mdbootstrap.com/img/Photos/Vertical/7.jpg" alt="project image">
+                </div>
+        
+                <div class="col-md-6 p-5 align-self-center">
+        
+                  <h5 class="font-weight-normal mb-3">Project detail</h5>
+        
+                  <p class="text-muted">Out or geared it but to best up samples the for she phase of copy would do in divine
+                    of taken and the take medical or upper at him in the logbook were, we price his mostly to commas.</p>
+        
+                  <ul class="list-unstyled font-small mt-5 mb-0">
+                    <li>
+                      <p class="text-uppercase mb-2"><strong>Client</strong></p>
+                      <p class="text-muted mb-4">Envato Inc.</p>
+                    </li>
+        
+                    <li>
+                      <p class="text-uppercase mb-2"><strong>Date</strong></p>
+                      <p class="text-muted mb-4">June 27, 2019</p>
+                    </li>
+        
+                    <li>
+                      <p class="text-uppercase mb-2"><strong>Skills</strong></p>
+                      <p class="text-muted mb-4">Design, HTML, CSS, Javascript</p>
+                    </li>
+        
+                    <li>
+                      <p class="text-uppercase mb-2"><strong>Address</strong></p>
+                      <a href="https://mdbootstrap.com/docs/jquery/design-blocks/">MDBootstrap</a>
+                    </li>
+        
+                    <li>
+                      <p class="text-uppercase mt-4 mb-2"><strong>Share</strong></p>
+                      <div class="d-flex justify-content-start">
+                        <a class="text-muted pr-3" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="text-muted pr-3" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="text-muted pr-3" href="#"><i class="fab fa-instagram"></i></a>
+                        <a class="text-muted" href="#"><i class="fab fa-dribbble"></i></a>
+                      </div>
+                    </li>
+        
+                  </ul>
+        
+                </div>
+        
+              </div>
+        
+            </div>
+        
+          </section>
       </div>
     </div>
   </div>
