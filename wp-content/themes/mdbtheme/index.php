359e88c0b95c9296dac1c406cab9768125048ccd
Giering <NB94559@DOW.COM>
2020-10-18 09:21:12 -0400

    asdf

--- wp-content/themes/mdbtheme/index.php
+++ wp-content/themes/mdbtheme/index.php
@@ -1154,7 +1154,7 @@
                 </div>
             </div>
   
-  <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+  <div class="modal fade paula-intro" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
