e5a0fc0ba244964045b1a0b5ea7838acd18acc66
Giering <NB94559@DOW.COM>
2020-10-22 07:31:30 -0400

    fewdrew

--- wp-content/themes/mdbtheme/components/navbar.inc.php
+++ wp-content/themes/mdbtheme/components/navbar.inc.php
@@ -1,75 +1,77 @@
 <!-- Navbar -->
 <header>
-    <!-- Navbar -->
-    <nav class="navbar white navbar-expand-lg navbar-light fixed-top" id="navbar-example">
-        <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand pt-0 waves-effect" href="">
-                    <img src="/wp-content/uploads/2020/09/logo-2.png" height="40" alt="Paula's Pet Sitting Logo">
-                </a>
-                <div class="collapse navbar-collapse" id="navbarToggler">
-                    <ul class="navbar-nav mr-auto ml-auto smooth-scroll">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about" data-offset="65">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#ourteam" data-offset="65">Our Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services" data-offset="65">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#faq" data-offset="65">FAQ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact" data-offset="65">Contact</a>
-                        </li>
-                    </ul>
-                    <!-- Social Icon  -->
-                    <ul class="navbar-nav nav-flex-icons">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.facebook.com/PaulasPetSittinginMidland/" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.instagram.com/paulaspetsittinginmidland/" target="_blank"><i class="fab fa-instagram"></i></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="mailto:PaulasPetSittingInMidlandMi@gmail.com" target="_blank"><i class="far fa-envelope"></i></a>
-                        </li>
-                    </ul>
-                </div>
-        </div>
-    </nav>
+
+<!-- Navbar -->
+<nav class="navbar white navbar-expand-lg navbar-light fixed-top" id="navbar-example">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+      aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <a class="navbar-brand pt-0 waves-effect" href="">
+            <img src="/wp-content/uploads/2020/09/logo-2.png" height="40" alt="Paula's Pet Sitting Logo">
+      </a>
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav mr-auto ml-auto smooth-scroll">
+        <li class="nav-item">
+          <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about" data-offset="65">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#ourteam" data-offset="65">Our Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#services" data-offset="65">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#faq" data-offset="65">FAQ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact" data-offset="65">Contact</a>
+        </li>
+        
+      </ul>
+      <!-- Social Icon  -->
+      <ul class="navbar-nav nav-flex-icons">
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.facebook.com/PaulasPetSittinginMidland/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.instagram.com/paulaspetsittinginmidland/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mailto:PaulasPetSittingInMidlandMi@gmail.com" target="_blank"><i class="far fa-envelope"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 </header>
 <section>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url(/wp-content/uploads/2020/10/paula-park-e1601762058686.jpg); background-repeat: no-repeat; background-size: cover; background-position: center center;">
-                <div class="mask rgba-black-light">
-                    <div class="container h-100 d-flex justify-content-center align-items-center">
-                        <div class="row pt-5 mt-3">
-                            <div class="col-md-12">
-                                <div class="text-center">
-                                    <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-3 wow fadeInDown" data-wow-delay="0.3s">
-                                        <strong>Looking for a pet sitter?</strong>
-                                    </h1>
-                                    <hr class="hr-light mt-4 wow fadeInDown" data-wow-delay="0.4s">
-                                    <h5 class=" mb-4 white-text wow fadeInDown" data-wow-delay="0.4s">
-                                        <strong>We are committed to providing professional in-home pet care services to the Midland community.</strong>
-                                    </h5>
-                                    <a class="btn btn-outline-white wow fadeInDown" href="tel:989-513-7937" data-wow-delay="0.4s">Call Now</a>
-                                    <a class="btn btn-outline-white wow fadeInDown" href="#contact" data-wow-delay="0.4s">Contact Us</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<div class="view jarallax" data-jarallax='{"speed": 0.2}'
+      style="background-image: url(/wp-content/uploads/2020/10/paula-park-e1601762058686.jpg); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+      <div class="mask rgba-black-light">
+        <div class="container h-100 d-flex justify-content-center align-items-center">
+          <div class="row pt-5 mt-3">
+            <div class="col-md-12">
+              <div class="text-center">
+                <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-3 wow fadeInDown"
+                  data-wow-delay="0.3s">
+                  <strong>Looking for a pet sitter?</strong>
+                </h1>
+                <hr class="hr-light mt-4 wow fadeInDown" data-wow-delay="0.4s">
+                <h5 class=" mb-4 white-text wow fadeInDown" data-wow-delay="0.4s">
+                  <strong>We are committed to providing professional in-home pet care services to the Midland community.</strong>
+                </h5>
+                <a class="btn btn-outline-white wow fadeInDown" href="tel:989-513-7937" data-wow-delay="0.4s">Call Now</a>
+                <a class="btn btn-outline-white wow fadeInDown" href="#contact" data-wow-delay="0.4s">Contact Us</a>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
 </section>
