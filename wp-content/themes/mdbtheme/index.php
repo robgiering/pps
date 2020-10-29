e226b0de47de268dc04266eed3281d85c070f4a3
Giering <NB94559@DOW.COM>
2020-10-29 16:19:51 -0400

    asdf

--- wp-content/themes/mdbtheme/index.php
+++ wp-content/themes/mdbtheme/index.php
@@ -1097,7 +1097,7 @@
                                 <!--Grid column-->
                                 <!--Grid column-->
                                 <div class="col-lg-7 col-md-12 mb-4 mb-md-0">
-                                    <?php echo do_shortcode( '[contact-form-7 id="9962" title="Contact form 1"]' ); ?>
+                                    <?php echo do_shortcode( '[contact-form-7 id="9962" title="Contact form 1" html_class="needs-validation"]' ); ?>
                                 </div>
                                 <!--Grid column-->
                             </div>
