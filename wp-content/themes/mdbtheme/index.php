e1038d2569f84c824d15e35d2bb6aaa4724dc03e
Giering <NB94559@DOW.COM>
2020-10-30 08:30:04 -0400

    asdf

--- wp-content/themes/mdbtheme/index.php
+++ wp-content/themes/mdbtheme/index.php
@@ -1100,7 +1100,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>a
+                                <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <section>
