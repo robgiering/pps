5098cdbbb87fd365e7c99fc025297d04d7e1c916
Giering <NB94559@DOW.COM>
2020-10-30 21:18:41 -0400

    test

--- wp-content/themes/mdbtheme/functions.php
+++ wp-content/themes/mdbtheme/functions.php
@@ -51,4 +51,20 @@ function mdb_widgets_init() {
 
 }
 add_action( 'widgets_init', 'mdb_widgets_init' );
+add_filter('wpcf7_form_elements', function( $content ) {
+  $dom = new DOMDocument();
+  $dom->preserveWhiteSpace = false;
+  $dom->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+  $xpath = new DomXPath($dom);
+  $spans = $xpath->query("//span[contains(@class, 'wpcf7-form-control-wrap')]" );
+
+  foreach ( $spans as $span ) :
+    $children = $span->firstChild;
+    $span->parentNode->replaceChild( $children, $span );
+  endforeach;
+
+  return $dom->saveHTML();
+});
+}
 ?>
